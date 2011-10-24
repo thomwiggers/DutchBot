25ee4d74da0735fb61c429783f4769cff24c071d
Thom Wiggers <thom.venividivici@gmail.com>
2011-10-24 00:57:58 +0000

    fixed some stuff, etc etc

--- DutchBot/src/org/Thom/DutchBot/DutchBot.java
+++ DutchBot/src/org/Thom/DutchBot/DutchBot.java
@@ -6,9 +6,7 @@ package org.Thom.DutchBot;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Timer;
-import java.util.TimerTask;
 
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
@@ -20,157 +18,164 @@ import org.jibble.pircbot.PircBot;
  */
 public class DutchBot extends PircBot {
 
-	/**
-	 * @Override
-	 */
-	private static final String VERSION = "0.5";
+    /**
+     * @Override
+     */
+    private static final String VERSION = "0.5";
 
-	/**
-	 * contains the password for nickserv
-	 */
-	private String _nickservPassword;
+    /**
+     * contains the password for nickserv
+     */
+    private String _nickservPassword;
 
-	/**
-	 * Channels the bot should autojoin
-	 */
-	private ArrayList<String> _autojoinChannels = new ArrayList<String>();
+    /**
+     * Channels the bot should autojoin
+     */
+    private ArrayList<String> _autojoinChannels = new ArrayList<String>();
 
-	/**
-	 * Timer Utility
-	 */
-	private Timer _timer = new Timer(true);
+    /**
+     * Timer Utility
+     */
+    private Timer _timer = new Timer(true);
 
-	/**
-	 * Initializes with default nick
-	 */
-	public DutchBot(String nickservPassword) {
-		this(nickservPassword, "DutchBot");
-	}
+    private final ConnectionProtectorTask _connectionProtector;
 
-	/**
-	 * Initializes bot.
-	 * 
-	 * @param name
-	 *            Nickname used when connecting.
-	 */
-	public DutchBot(String nickservPassword, String name) {
-		this.setNickservPassword(nickservPassword);
-		this.setName(name);
-		setVersion(name + " " + VERSION + " by DutchDude");
-		setLogin("name");
-		
-		this.getTimer().schedule(new ConnectionProtectorTask(this), 1000L, 1000L);
-	}
+    /**
+     * Initializes with default nick
+     */
+    public DutchBot(String nickservPassword) {
+	this(nickservPassword, "DutchBot");
+    }
 
-	/**
-	 * Try to connect to the server, return the result
-	 * 
-	 * @param server
-	 * @param port
-	 * @param password
-	 * @param
-	 * @return Are we connected now?
-	 * @throws IOException
-	 * @throws IrcException
-	 * @throws InterruptedException
-	 */
-	public final boolean tryConnect(String server, int port, final String nick,
-			String password) throws IOException, IrcException,
-			InterruptedException {
-		if (!this.isConnected()) {
-			try {
-				this.setName(nick);
-				this.setAutoNickChange(true);
-				this.connect(server, port, password);
-				GhostTask gt = new GhostTask(this.getTimer(), this, nick);
-				if (this.getNick() != nick) {
-					this.getTimer().schedule(gt, 1000L);
-				}
-				this.changeNick(nick);
-			} catch (NickAlreadyInUseException e) {
-				System.err
-						.print("********************** ERROR ****************************\n");
-				System.err
-						.print(" ->  Nick already in use - switching nicks failed\n\n");
+    /**
+     * Initializes bot.
+     * 
+     * @param name
+     *            Nickname used when connecting.
+     */
+    public DutchBot(String nickservPassword, String name) {
+	this.setNickservPassword(nickservPassword);
+	this.setName(name);
+	this.setVersion(name + " " + VERSION + " by DutchDude");
+	this.setLogin("name");
+	_connectionProtector = new ConnectionProtectorTask(this);
+	this.getTimer().schedule(_connectionProtector, 1000L, 1000L);
+    }
 
-				return false;
-			}
-			this.identify(this.getNickservPassword());
-
-			this.autoJoin();
-			this.sendMessage("DutchDude", "I'm here!");
-			
+    /**
+     * Try to connect to the server, return the result
+     * 
+     * @param server
+     * @param port
+     * @param password
+     * @param
+     * @return Are we connected now?
+     * @throws IOException
+     * @throws IrcException
+     * @throws InterruptedException
+     */
+    public final boolean tryConnect(String server, int port, final String nick,
+	    String password) throws IOException, IrcException,
+	    InterruptedException {
+	if (!this.isConnected()) {
+	    try {
+		this.setName(nick);
+		this.setAutoNickChange(true);
+		this.connect(server, port, password);
+		GhostTask gt = new GhostTask(this, nick);
+		if (this.getNick() != nick) {
+		    this.getTimer().schedule(gt, 1000L);
 		}
-		return this.isConnected();
-	}
+		this.changeNick(nick);
+	    } catch (NickAlreadyInUseException e) {
+		System.err
+			.print("********************** ERROR ****************************\n");
+		System.err
+			.print(" ->  Nick already in use - switching nicks failed\n\n");
 
-	private final void autoJoin() {
-		for (int i = 0; i < this._autojoinChannels.size(); i++) {
-			this.joinChannel(this._autojoinChannels.get(i));
-		}
-	}
+		return false;
+	    }
+	    this.identify(this.getNickservPassword());
 
-	/**
-	 * NickServ-GHOSTs a nick using the NickServ password
-	 * 
-	 * @param nick
-	 */
-	final void ghost(String nick) {
-		this.sendRawLine("NickServ GHOST " + nick + " "
-				+ this.getNickservPassword());
-	}
+	    this.autoJoin();
+	    this.sendMessage("DutchDude", "I'm here!");
 
-	@Override
-	protected void onInvite(String targetNick, String sourceNick,
-			String sourceLogin, String sourceHostname, String channel) {
-		System.out.println("Invited to channel " + channel);
-		System.out.println("The sourcehostname: " + sourceHostname);
-		if (targetNick.equals(this.getNick())
-				&& AccessList.isAllowed(sourceHostname, Privileges.OPERATOR)) {
-			// TODO match admins
-			this.joinChannel(channel);
-		}
 	}
+	return this.isConnected();
+    }
 
-	public void addAutoJoinList(String channel) {
-		this._autojoinChannels.add(channel);
+    private final void autoJoin() {
+	for (int i = 0; i < this._autojoinChannels.size(); i++) {
+	    this.joinChannel(this._autojoinChannels.get(i));
 	}
+    }
 
-	public void setAutoJoinList(String[] channels) {
-		this._autojoinChannels = new ArrayList<String>(Arrays.asList(channels));
-	}
+    /**
+     * NickServ-GHOSTs a nick using the NickServ password
+     * 
+     * @param nick
+     */
+    final void ghost(String nick) {
+	this.sendRawLine("NickServ GHOST " + nick + " "
+		+ this.getNickservPassword());
+    }
 
-	/**
-	 * Gets the nickserv password
-	 * 
-	 * @return String
-	 */
-	private final String getNickservPassword() {
-		return _nickservPassword;
+    @Override
+    protected void onInvite(String targetNick, String sourceNick,
+	    String sourceLogin, String sourceHostname, String channel) {
+	System.out.println("Invited to channel " + channel);
+	System.out.println("The sourcehostname: " + sourceHostname);
+	if (targetNick.equals(this.getNick())
+		&& AccessList.isAllowed(sourceHostname, Privileges.OPERATOR)) {
+	    this.joinChannel(channel);
 	}
+    }
 
-	/**
-	 * Sets the nickserv password
-	 * 
-	 * @param nickservPassword
-	 */
-	public final void setNickservPassword(String nickservPassword) {
-		this._nickservPassword = nickservPassword;
-	}
+    @Override
+    protected void onMessage(String channel, String sender, String login,
+	    String hostname, String message) {
 
-	/**
-	 * @return the timer
-	 */
-	public Timer getTimer() {
-		return _timer;
-	}
+    }
 
-	/**
-	 * @param timer
-	 *            the timer to set
-	 */
-	public void setTimer(Timer timer) {
-		this._timer = timer;
-	}
+    public void addAutoJoin(String channel) {
+	this._autojoinChannels.add(channel);
+    }
+
+    public void setAutoJoinList(String[] channels) {
+	this._autojoinChannels = new ArrayList<String>(Arrays.asList(channels));
+    }
+
+    /**
+     * Gets the nickserv password
+     * 
+     * @return String
+     */
+    private final String getNickservPassword() {
+	return _nickservPassword;
+    }
+
+    /**
+     * Sets the nickserv password
+     * 
+     * @param nickservPassword
+     */
+    public final void setNickservPassword(String nickservPassword) {
+	this._nickservPassword = nickservPassword;
+    }
+
+    /**
+     * @return the timer
+     */
+    public Timer getTimer() {
+	return _timer;
+    }
+
+    /**
+     * @param timer
+     *            the timer to set
+     */
+    public void setTimer(Timer timer) {
+	this._timer = timer;
+    }
 
 }
--- DutchBot/src/org/Thom/DutchBot/GhostTask.java
+++ DutchBot/src/org/Thom/DutchBot/GhostTask.java
@@ -1,31 +1,25 @@
 package org.Thom.DutchBot;
 
-import java.util.Timer;
 import java.util.TimerTask;
 
 class GhostTask extends TimerTask {
-	
-	private Timer timer;
-	private DutchBot bot;
-	private String nick;
-	
-	
-	GhostTask(Timer timer, DutchBot bot, String nick) {
-		super();
-		this.bot = bot;
-		this.nick = nick;
+    private final DutchBot bot;
+    private final String nick;
+
+    GhostTask(DutchBot bot, String nick) {
+	super();
+	this.bot = bot;
+	this.nick = nick;
+    }
+
+    @Override
+    public void run() {
+	if (nick != bot.getNick()) {
+	    bot.ghost(nick);
+	    bot.changeNick(nick);
+	    GhostTask gt = new GhostTask(bot, nick);
+	    this.bot.getTimer().schedule(gt, 1000L);
 	}
 
-
-	@Override
-	public void run() {
-		if (nick != bot.getNick())
-		{
-			bot.ghost(nick);
-			bot.changeNick(nick);
-			this.timer.schedule(this, 1000L);
-		}
-
-	}
-
+    }
 }
