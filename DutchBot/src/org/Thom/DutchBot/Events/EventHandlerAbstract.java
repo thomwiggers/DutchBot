09cbfaeef76dd9f0f3bd83d209e13c7dc3d0c8da
DutchDude <SendMeAPM@What.CD>
2011-11-02 21:38:36 +0100

    Refactored messages

--- DutchBot/src/org/Thom/DutchBot/Events/EventHandlerAbstract.java
+++ DutchBot/src/org/Thom/DutchBot/Events/EventHandlerAbstract.java
@@ -1,6 +1,6 @@
 package org.Thom.DutchBot.Events;
 
-public abstract class Event {
+public abstract class EventHandlerAbstract {
     @Override
     public String toString() {
 	return this.getClass().getName();
