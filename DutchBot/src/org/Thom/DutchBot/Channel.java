5d72b9c8cdf7d099f008f1b22f6bcec00cfc6764
DutchDude <SendMeAPM@What.CD>
2011-11-09 22:49:12 +0100

    Eerste opzet naar logging

--- DutchBot/src/org/Thom/DutchBot/Channel.java
+++ DutchBot/src/org/Thom/DutchBot/Channel.java
@@ -3,6 +3,8 @@
  */
 package org.Thom.DutchBot;
 
+import java.util.Calendar;
+
 import org.jibble.pircbot.User;
 
 /**
@@ -16,10 +18,22 @@ public class Channel {
     private final DutchBot _bot;
     private boolean joined = false;
 
+    private ChannelLogger _logger;
+
+    public void startLog(String id) {
+	this._logger.startLog(id + " - " + this._channelName + " - "
+		+ Calendar.getInstance().getTimeInMillis());
+    }
+
+    public void closeLog() {
+	this._logger.closeLog();
+    }
+
     public Channel(DutchBot bot, String name) {
 	this._bot = bot;
 	this._key = null;
 	this._channelName = name;
+	this._logger = new ChannelLogger(this);
     }
 
     public Channel(DutchBot bot, String name, String key) {
@@ -33,6 +47,11 @@ public class Channel {
 	    this._bot.joinChannel(this._channelName);
 	else
 	    this._bot.joinChannel(_channelName, _key);
+    }
+
+    public void processMessage(String sender, String login, String host,
+	    String message) {
+	this._logger.writeLog(sender, login, host, message);
     }
 
     public void hasJoined() {
