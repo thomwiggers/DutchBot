286397829706b2fd9a5e3d916197d42532c96680
DutchDude <SendMeAPM@What.CD>
2011-11-09 16:58:35 +0100

    Added base for channel management

---
+++ DutchBot/src/org/Thom/DutchBot/Channel.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.Thom.DutchBot;
+
+import org.jibble.pircbot.User;
+
+/**
+ * @author Thom
+ * 
+ */
+public class Channel {
+
+    private final String _channelName;
+    private final String _key;
+    private final DutchBot _bot;
+    private boolean joined = false;
+
+    public Channel(DutchBot bot, String name) {
+	this._bot = bot;
+	this._key = null;
+	this._channelName = name;
+    }
+
+    public Channel(DutchBot bot, String name, String key) {
+	this._bot = bot;
+	this._key = key;
+	this._channelName = name;
+    }
+
+    public void join() {
+	if (_key == null)
+	    this._bot.joinChannel(this._channelName);
+	else
+	    this._bot.joinChannel(_channelName, _key);
+    }
+
+    public void hasJoined() {
+	this.joined = true;
+    }
+
+    public User[] getUsers() {
+	return this._bot.getUsers(this._channelName);
+    }
+
+    public User getUser(String nick) {
+	for (User user : this.getUsers()) {
+	    System.out.println(user.getNick());
+	    if (user.getNick().equalsIgnoreCase(nick))
+		return user;
+	}
+	return null;
+    }
+
+}
