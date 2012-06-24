a84d38e0b6376790c1dd930cd78c638da19d4957
Thom Wiggers <ret@rded.nl>
2012-06-24 02:54:29 +0200

    Deleted Events
    Deleted the events, using Modules now anyway

--- src/org/Thom/DutchBot/Events/JoinEventHandlerAbstract.java
+++
@@ -1,8 +0,0 @@
-package org.Thom.DutchBot.Events;
-
-import org.Thom.DutchBot.DutchBot;
-
-public abstract class JoinEventHandlerAbstract extends EventHandlerAbstract {
-    public abstract boolean run(DutchBot bot, String channel, String nick,
-	    String login, String hostname);
-}
